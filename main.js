f2c65885479a390505275ceedb234961328e4b4c
gabriel010706 <128716288+gabriel010706@users.noreply.github.com>
2024-03-21 08:25:10 -0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll(".aba-conteudo")
 
+for(let i=0) <botoes.length; 1++){
+    botoes[1].onclick =function(){
+        for(let j=09;-j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
+
+        }
+}
+botoes[1].classList.add("ativo");
+textos[1].classlist.add("ativo")
+
+
+}
